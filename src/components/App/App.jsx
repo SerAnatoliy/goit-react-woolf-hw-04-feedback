ed632911d18402424780706c3c95eeda720faea5
SerAnatoliy <anatolii@maximustribe.com>
2024-03-18 21:49:42 +0100

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import { Section } from '../Section/Section';
-import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
-import { Statistics } from '../Statistics/Statistics';
-import { Notification } from '../Notification/Notification';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 import { Container } from './App.styled';
 
 export const App = () => {
